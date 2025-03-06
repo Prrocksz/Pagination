317c3585ad2cd9f7c6715bc323f199500f776c14
prashantsuyal <prashant_96@outlook.com>
2025-03-06 18:59:03 +0530

    Made Changes

--- Pagination/Models/Employee.cs
+++ Pagination/Models/Employee.cs
@@ -1,6 +1,14 @@
 namespace Pagination.Models;
 
-public class Employee
+public class Employees
 {
+    public int Id { get; set; }
     
+    public string FirstName { get; set; }
+    
+    public string LastName { get; set; }
+    
+    public string PhoneNumber { get; set; }
+    
+    public int Salary { get; set; }
 }
